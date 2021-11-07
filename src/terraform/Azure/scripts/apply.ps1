e6dfb85fa72528df08f17588e341a507e5eb3497
Josh Rolstad <jorolsta@microsoft.com>
2021-11-07 08:23:39 -0800

    Adding scripts to enable development

---
+++ src/terraform/Azure/scripts/apply.ps1
@@ -0,0 +1,8 @@
+$serviceName = "aadlicense"
+$environment = "dev"
+
+terraform apply -input=false `
+    -auto-approve `
+    -var="service_name=${serviceName}" `
+    -var-file=".\config\${serviceName}.tfvars" `
+    -var-file=".\config\${environment}.tfvars"
---
+++ src/terraform/Azure/scripts/destroy.ps1
@@ -0,0 +1,9 @@
+$serviceName = "aadlicense"
+$environment = "dev"
+
+terraform apply -input=false `
+    -auto-approve `
+    -destroy `
+    -var="service_name=${serviceName}" `
+    -var-file=".\config\${serviceName}.tfvars" `
+    -var-file=".\config\${environment}.tfvars"
